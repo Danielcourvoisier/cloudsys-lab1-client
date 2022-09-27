5a2ad62b10f528908bf4e774eb3b20a64ebdbce2
Daniel Courvoisier <daniel.courvoisier@protonmail.ch>
2022-09-27 11:06:41 +0200

    m

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Install dependencies
-pip install
-
+# To install on start of server
+#sudo apt -y install git
+#cd /home/ubuntu/
+#git clone https://github.com/Danielcourvoisier/cloudsys-lab1-client.git
+#export CLOUDSYS_SERVER_IP=google.ch
+export CLOUDSYS_SERVER_IP=194.182.190.225:8080 >>~/.bashrc
 # Run Client
-python3 main.py 
+cd /home/ubuntu/cloudsys-lab1-client/
+python3 main.py
--- main.py
+++ main.py
@@ -10,9 +10,9 @@ server_addr = os.environ["CLOUDSYS_SERVER_IP"]
 print(f"Server address is : {server_addr}")
 
 while True:
-    response_API = requests.get(f"http://{server_addr}")
-    print(response_API.status_code)
-    data = response_API.text
-    print(data)
+    try:
+        response_API = requests.get(f"http://{server_addr}")
+        print(response_API.status_code)
+        data = response_API.text
+        print(data)
     time.sleep(1)
-    #json.loads(data)
