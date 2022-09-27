adacc3c49b3040d864d7b725569670b83725f14d
Daniel Courvoisier <daniel.courvoisier@protonmail.ch>
2022-09-27 09:45:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import json
 # Get env variable to get IP of server
 server_addr = os.environ["CLOUDSYS_SERVER_IP"]
 
+print(f"Server address is : {server_addr}")
+
 while True:
     response_API = requests.get(f"http://{server_addr}")
     print(response_API.status_code)
