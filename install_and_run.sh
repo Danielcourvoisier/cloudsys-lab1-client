c4a2a1d7d4063a8c66cfd3bbabccf28bc94e1fe2
Daniel Courvoisier <daniel.courvoisier@protonmail.ch>
2022-09-27 09:42:44 +0200

    Add env var support

---
+++ install_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install dependencies
+pip install
+
+# Run Client
+python3 main.py 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Dependancies
+import os
 import time
-
 import requests
 import json
 
+# Get env variable to get IP of server
+server_addr = os.environ["CLOUDSYS_SERVER_IP"]
 
 while True:
-    response_API = requests.get('http://127.0.0.1:8000/')
+    response_API = requests.get(f"http://{server_addr}")
     print(response_API.status_code)
     data = response_API.text
     print(data)
